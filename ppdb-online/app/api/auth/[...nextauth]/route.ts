c1d2d524f9f2d2cbba66cd8d5d7df28d20a83ce2
Fikri <fikri.hakim.13@gmail.com>
2025-07-13 17:13:16 +0700

    Fix console error // [next-auth][error][CLIENT_FETCH_ERROR] "\nhttps://next-auth.js.org/errors#client_fetch_error" "Failed to execute 'json' on 'Response': Unexpected end of JSON input" {} Fix by edit route.ts.

--- ppdb-online/app/api/auth/[...nextauth]/route.ts
+++ ppdb-online/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,33 @@
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+
+// const handler = NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//   ],
+//   secret: process.env.NEXTAUTH_SECRET,
+// });
+
+// // export { handler as GET, handler as POST };
+// export const authOptions = {
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID as string,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+//     }),
+//   ],
+//   secret: process.env.NEXTAUTH_SECRET,
+// };
+// // Should I move this file to ..\pages\api ? NO.
+
+// Fix console error
+// [next-auth][error][CLIENT_FETCH_ERROR] "\nhttps://next-auth.js.org/errors#client_fetch_error" "Failed to execute 'json' on 'Response': Unexpected end of JSON input" {}
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-});
-
-// export { handler as GET, handler as POST };
 export const authOptions = {
   providers: [
     GoogleProvider({
@@ -21,4 +37,8 @@ export const authOptions = {
   ],
   secret: process.env.NEXTAUTH_SECRET,
 };
-// Should I move this file to ..\pages\api ? NO.
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
+
